180f054bd554528df7129e1fc0543c0fe2ed89bc
Lassilos <71727271+Lassilos@users.noreply.github.com>
2026-01-13 23:39:13 +0100

    Delete src directory

--- src/client/java/org/lassilos/color/mixin/client/InventoryScreenMixin.java
+++
@@ -1,4 +0,0 @@
-// InventoryScreenMixin replaced by SurvivalInventoryMixin; placeholder kept to avoid accidental references.
-package org.lassilos.color.mixin.client;
-
-// Placeholder file left intentionally empty. See SurvivalInventoryMixin.java for actual implementation.
